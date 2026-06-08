0c71975f0e3c470a52ddccf7b7cf8b6586227bcf
Colin Graydon <colingraydon@gmail.com>
2026-06-07 22:56:44 -0400

    Initial Commit

---
+++ Delve/DelveApp.swift
@@ -0,0 +1,17 @@
+//
+//  DelveApp.swift
+//  Delve
+//
+//  Created by Colin Graydon on 6/7/26.
+//
+
+import SwiftUI
+
+@main
+struct DelveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
